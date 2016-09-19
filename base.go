7bf77ef40a116889b43a0538d230a5eee5fe91d2
Andre Sutherland <andrelsf95@gmail.com>
2016-09-19 14:50:05 +0100

    Removed RenderMultiple from template provider interface.

--- base.go
+++ base.go
@@ -5,7 +5,6 @@ import "io"
 type Provider interface {
 	Render(io.Writer, string, interface{}) error
 	RenderWithLayout(io.Writer, string, string, interface{}) error
-	RenderMultiple(io.Writer, []string, interface{}) error
 	HasTemplate(tmpl string) bool
 	Invalidate()
 }
